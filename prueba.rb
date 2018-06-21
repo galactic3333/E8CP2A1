e61ffb6350fc40bd508b5bb95171e494ab522708
Gabriel Alarcon <galarconesp@gmail.com>
2018-06-21 18:11:58 -0400

    commit 02

--- prueba.rb
+++ prueba.rb
@@ -20,32 +20,32 @@ while option != '4'
     alumnos = file.readlines.map(&:chomp).map { |w| w.split(', ') }
     file.close
 
-    print alumnos
+    # print alumnos
 
     soloalumnos = alumnos.map { |lista| lista[0] }
-    print soloalumnos
+    # print soloalumnos
 
     notasyausente = alumnos.each { |lista| lista.delete_at(0) }
-    print notasyausente
+    # print notasyausente
 
     notas = notasyausente.map { |array| array.map { |x| x.to_i if x.is_integer? } }
-    print notas
+    # print notas
 
     solonotas = notas.map(&:compact)
 
-    print solonotas
+    # print solonotas
 
     promedio = solonotas.map{ |notasalum| notasalum.inject { |sum, x| sum + x} / notasyausente.length.to_f }
 
-    print promedio
+    # print promedio
 
     alumno_promedio = soloalumnos.zip(promedio).to_h
 
-    print alumno_promedio
+    # print alumno_promedio
 
     file = File.open('promedio_alumnos.txt', 'w')
 
-    alumno_promedio.each { |key,value| file.puts "#{key} #{value}"}
+    alumno_promedio.each { |key, value| file.puts "#{key} #{value}" }
 
     file.close
   end
@@ -55,74 +55,69 @@ while option != '4'
     alumnos = file.readlines.map(&:chomp).map { |w| w.split(', ') }
     file.close
 
-    #print alumnos
+    # print alumnos
 
     soloalumnos = alumnos.map { |lista| lista[0] }
-    #print soloalumnos
+    # print soloalumnos
 
     notasyausente = alumnos.each { |lista| lista.delete_at(0) }
-    
-   #print notasyausente
+
+    # print notasyausente
 
     soloa = notasyausente.map { |lista| lista.map { |x| x if x == 'A' } }
 
-    #print soloa
+    # print soloa
 
     ausente = soloa.map(&:compact)
 
-    #print ausente
+    # print ausente
 
     numaus = ausente.map{ |lista| lista.inject(0) { |sum, x| sum + 1 if x == 'A'} }
 
-    print numaus
+    # print numaus
 
-    totaus = numaus.inject(0) { |sum, x| sum + x}
+    totaus = numaus.inject(0) { |sum, x| sum + x }
 
-    print totaus
+    # print totaus
     alumno_ausente = soloalumnos.zip(numaus).to_h
 
-    print alumno_ausente
+    # print alumno_ausente
 
-    puts "\n\nLa cantidad de Insistencias totales del curso es #{totaus} inasistencias"
+    puts "\n\nLa cantidad de Insistencias totales del curso es #{totaus} inasistencia#{totaus != 1 ? 's': ''}"
     puts 'El detalle de inasistencias por alumno es el siguiente:'
-    alumno_ausente.each{ |key, value| print "\n#{key}: #{value} inasistencias"}
-
+    alumno_ausente.each { |key, value| print "\n#{key}: #{value} inasistencia#{value != 1 ? 's': ''}" }
   end
 
   def approved(arg, arg2)
-
     file = File.open(arg, 'r')
     alumnos = file.readlines.map(&:chomp).map { |w| w.split(', ') }
     file.close
 
-    print alumnos
+    # print alumnos
 
     soloalumnos = alumnos.map { |lista| lista[0] }
-    print soloalumnos
+    # print soloalumnos
 
     notasyausente = alumnos.each { |lista| lista.delete_at(0) }
-    print notasyausente
+    # print notasyausente
 
     notas = notasyausente.map { |array| array.map { |x| x.to_i if x.is_integer? } }
-    print notas
+    # print notas
 
     solonotas = notas.map(&:compact)
 
-    print solonotas
+    # print solonotas
 
     promedio = solonotas.map{ |notasalum| notasalum.inject { |sum, x| sum + x} / notasyausente.length.to_f }
 
-    print promedio
+    # print promedio
 
     alumno_promedio = soloalumnos.zip(promedio).to_h
 
-    print alumno_promedio
+    # print alumno_promedio
 
     alumno_promedio.each { |key, value| puts "#{key} esta aprobado con nota #{value}" if value >= arg2 }
-
   end
-
-
 
   case option
 
@@ -131,7 +126,16 @@ while option != '4'
   when '2'
     no_attendance('alumnos.csv')
   when '3'
-    approved('alumnos.csv',5)
+    puts 'La nota necesaria para aprobar por defecto es 5, si deseas cambiarla ingresa si, para continuar presiona enter:'
+    inputu = gets.chomp
+    if inputu.downcase == 'si'
+      puts 'Ingresa la nueva nota necesaria para aprobar:'
+      notapr = gets.chomp.to_f
+    else
+      notapr = 5
+    end
+
+    approved('alumnos.csv', notapr)
   end
 
   validoption = ['1', '2', '3', '4']
@@ -149,7 +153,5 @@ while option != '4'
   puts "4\tSalir"
 
   option = gets.chomp
-
-
 
 end
